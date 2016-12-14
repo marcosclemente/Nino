05d37437cb2ea03a4497aa6b1bf7a6fbb3cf4dee
paulamarangoni <paula_marangoni@yahoo.com.br>
2016-12-14 16:44:29 -0200

    Add biblio math e modificado valor de 2pi e alterado o loop.

--- grafico_elnino.py
+++ grafico_elnino.py
@@ -1,35 +1,40 @@
 import matplotlib.pyplot as plt 
+import math as math
 
-# Numero do tempo, tipo 60 min ou seg, mas não sei se ta certo, dps a gente vê
-t = 61
-
-# Nosso primeiro angulo, zero
-x = 0
+giro = int(math.degrees((2*math.pi) + (math.pi/180)))
+# lista vazia de t
+tempo = []
 
 # A lista fazia que dps vai conter os resultados dos senos, é a nossa f(x)
 eixo_y = []
 
+# Nosso primeiro angulo, zero
+x = 0
+
 
 # Um for pra calcular os senos, ai eu botei 361, pra ir ate 360. Mas to na duvida se o valor do angulo tem que ser em radiano.
-for t in range(0,61):
-    for x in range(0, 361):
-        f(x) = sin(x)
+for t in range(0, 61):
+    tempo.append(t)
+
+# ta de 6 em 6 que é pra cada valor de tempo ter um valor de seno
+for x in range(0, giro, 6):
+    y = int(math.sin(x))
     # Add os resultados dos senos na lista
-        eixo_y = eixo_y.append(f(x))
+    eixo_y.append(y)
 
 # Fazendo o gráfico
 
-plt.figure()
+    plt.figure()
 
-plt.plot(range(0, t, 1), eixo_y, "-k")
+    plt.plot(tempo, eixo_y, "-k")
 
-plt.title ("Grafico_SENO")
-plt.xlabel("Tempo")
-plt.ylabel("Resultados_SENO")
+    plt.title ("Grafico_SENO")
+    plt.xlabel("Tempo")
+    plt.ylabel("Resultados_SENO")
 
-plt.savefig("fig/grafico.gif")
+    plt.savefig("fig/grafico.gif")
 
-plt.close()
+    plt.close()
 
 # Ainda não rodei, se vc tiver com o jupyter, tenta rodar pra ver como ta, acho que o grafico ta sobrescrevendo, pq falta uma parte, essa aqui
 
